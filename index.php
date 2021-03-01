29bdcb7356eb19bb55f2eb51b5d4014d451e8874
Ndeye-Lika-Cisse <ndeyelika19@gmail.com>
2021-03-01 17:34:48 +0000

    AddMVC

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+require_once "config/autoload.php";
+use src\controller\RolesController;
+
+
+$mvc = new libs\system\BootStrap();
+//$rolesdao = new RolesController();
+//$rolesdao->add();
+//$rolesdao->getAll();
+
+
+?>
